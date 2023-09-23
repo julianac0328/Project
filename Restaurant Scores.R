f6c5418e8b5f2a7ab9f9e4c6fb50badcd62dec3c
julianac0328 <145041665+julianac0328@users.noreply.github.com>
2023-09-23 11:29:35 -0400

    Add files via upload

---
+++ Restaurant Scores.R
@@ -0,0 +1,5 @@
+library(readr)
+scores <- read_csv("Downloads/Restaurant_Scores_-_LIVES_Standard.csv")
+View(scores)
+list(hist(scores$inspection_score))
+boxplot(scores$inspection_score,horizontal = TRUE, main = "Restaurant Scores", xlab = "Inspection Scores")
