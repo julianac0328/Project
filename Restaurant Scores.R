7298a1a0837fc7f108e6a1cd015787fa3d596f26
julianac0328 <145041665+julianac0328@users.noreply.github.com>
2023-10-20 16:56:50 -0400

    Restaurant Scores.R

--- Restaurant Scores.R
+++ Restaurant Scores.R
@@ -3,3 +3,6 @@ scores <- read_csv("Downloads/Restaurant_Scores_-_LIVES_Standard.csv")
 View(scores)
 list(hist(scores$inspection_score))
 boxplot(scores$inspection_score,horizontal = TRUE, main = "Restaurant Scores", xlab = "Inspection Scores")
+list(hist(scores$Neighborhoods))
+boxplot(scores$Neighborhoods,horizontal = TRUE, main = "Neighborhoods". xlab = "Neighborhood Numbers")
+
