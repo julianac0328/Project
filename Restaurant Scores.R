1fe9a584e2043a851e71b9d47b4b398628ffded6
julianac0328 <145041665+julianac0328@users.noreply.github.com>
2023-10-20 17:10:30 -0400

    Restaurant Scores.R

--- Restaurant Scores.R
+++ Restaurant Scores.R
@@ -4,5 +4,5 @@ View(scores)
 list(hist(scores$inspection_score))
 boxplot(scores$inspection_score,horizontal = TRUE, main = "Restaurant Scores", xlab = "Inspection Scores")
 list(hist(scores$Neighborhoods))
-boxplot(scores$Neighborhoods,horizontal = TRUE, main = "Neighborhoods". xlab = "Neighborhood Numbers")
+boxplot(scores$Neighborhoods,horizontal = TRUE, main = "Neighborhoods", xlab = "Neighborhood Numbers")
 
